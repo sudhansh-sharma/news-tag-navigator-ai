7b2816d3769775bc4c57f83de6abb9a3a3e6e799
AJ-bnb <bubsnbiba@gmail.com>
2025-05-29 14:10:31 +0530

    Updated Permissions

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -21,4 +21,4 @@ fi
 python manage.py collectstatic --noinput
 
 # Start Gunicorn
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3 
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
